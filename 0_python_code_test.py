58d3060717d484506986cba15f13243d0a0e7104
Tarun Goyal <tarun13317@gmail.com>
2018-09-17 13:02:06 +0530

    Student and Human class implemented demonstrating collaboration

--- 0_python_code_test.py
+++ 0_python_code_test.py
@@ -33,13 +33,16 @@ def getPrimes():
         if flag==1:
             yield num
 
-class User:
+class Student:
 
-    def __init__(self, id, name, age, course):
+    def __init__(self, id, name, age, course, human):
         self.id = id
         self.name = name
         self.age = age
         self.course = course
+
+        # here human is object of type human
+        self.human = human
 
     def setInterests(self, interestList):
         self.interests = interestList
@@ -60,14 +63,64 @@ class User:
     def getInterests(self):
         return self.interests
 
-    def getUserDetails(self):
+    def getStudDetails(self):
+        alldetails = {}
+
+        alldetails['id'] = self.getid()
+        alldetails['name'] = self.getname()
+        alldetails['age'] = self.getage()
+        alldetails['course'] = self.getcourse()
+        alldetails['interests'] = self.getInterests()
+
+        for tkey, tvalue in self.human.getHumanDetails().items():
+            alldetails[tkey] = tvalue
+
+        return alldetails
+
+
+class Human:
+
+    def __init__(self, h, w, ec, sc, qrc):
+
+        if not h.isdigit() or not w.isdigit():
+            raise ValueError("Entered height {}  or weight {} is not numeric".format(h,w))
+        if not ec.isalpha() or not sc.isalpha():
+            raise ValueError("Entered eyecolor {} or skin color {} is not string".format(ec, sc))
+        if not qrc[:2].isdigit() or not  qrc[2:].isalpha():
+            raise ValueError("Entered qrcode for human is not valid".format(qrc))
+
+        self._height = h
+        self._weight = w
+        self._eyecolor = ec
+        self._skincolor = sc
+        self._qrcode = qrc
+
+    def getHeight(self):
+        return self._height
+
+    def getWeight(self):
+        return self._weight
+
+
+    def getEyecolor(self):
+        return self._eyecolor
+
+    def getSkincolor(self):
+        return self._skincolor
+
+    def getQRcode(self):
+        return self._qrcode
+
+    def getHumanDetails(self):
         return {
-                'id':self.getid(),
-                'name':self.name,
-                'age':self.getage(),
-                'course':self.getcourse(),
-                'interests':self.getInterests()
-        }
+            'height': self.getHeight(),
+            'weight': self.getWeight(),
+            'eyecolor': self.getEyecolor(),
+            'skincolor': self.getSkincolor(),
+            'qrcode': self._qrcode
+            }
+
+
 
 
 if __name__ == '__main__':
@@ -88,21 +141,19 @@ if __name__ == '__main__':
 
     ##########################################################
 
-    id  = int(input("Enter id:"))
-    name  = str(input("Enter name:"))
-    age  = int(input("Enter age:"))
-    course  = str(input("Enter course:"))
 
-    user = User(1, 'Tarun', 22, 'MCA')
+    h = Human(input("Enter height:"),input("Enter weight:"),input("Enter eyecolor:"),input("Enter skincolor:"),input("Enter qrcode:"))
 
-    user.setInterests([i.strip() for i in str(input('Enter interests(, separated):')).split(',')])
+    student  = Student(int(input("Enter id:")), str(input("Enter name:")), int(input("Enter age:")), str(input("Enter course:")), h)
 
-    userDetails = user.getUserDetails()
+    student.setInterests([i.strip() for i in str(input('Enter interests(, separated):')).split(',')])
+
+    studentDetails = student.getStudDetails()
 
 
-
-    for key, value in userDetails.items():
+    for key, value in studentDetails.items():
         if key=='interests':
+            print(key,':')
             for v in value:
                 # v is an interest
                 print(v)
